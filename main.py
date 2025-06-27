5fd777d060898369038f10b62a548f6788ed7956
GAURAV N V <gauravnagalapuram@gmail.com>
2025-06-27 08:20:38 -0700

    feat: Admin pannel

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def HomePage():
     }
 
     return render_template("index.html", **data)
+
+
+@app.route("/admin")
+def AdminPage():
+    data = {
+        "pages": ["About", "Works", "Projects", "Publications", "Contact"],
+    }
+
+    return render_template("admin_page.html", **data)
