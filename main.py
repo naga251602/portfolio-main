3809206e80a20aed50788a640271cc198adab8c4
GAURAV N V <gauravnagalapuram@gmail.com>
2025-06-26 21:02:32 -0700

    feat: production ready

--- main.py
+++ main.py
@@ -8,9 +8,5 @@ def HomePage():
     data = {
         "pages": ["About", "Works", "Projects", "Publications", "Contact"],
     }
-    
+
     return render_template("index.html", **data)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
